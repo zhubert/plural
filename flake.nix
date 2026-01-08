81aaf291666080e1ea8a083b30626f5f4be442f9
Zack Hubert <zhubert@gmail.com>
2026-01-08 09:23:06 -0800

    Bump version to v0.0.11

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        version = "0.0.10";
+        version = "0.0.11";
       in
       {
         packages = {
